c77d4a975082680a7fcbea705b927b6c471f166b
Ilker Manap <ilker.manap@scania.com>
2023-09-14 12:44:48 +0200

    Updated file names in main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from topology import Topology
 
-c1 = open("ibnetdiscover-nash-compute.txt").readlines()
-c2 = open("ibnet.txt").readlines()
+c1 = open("ibnetdiscover-output.txt").readlines()
 
 t = Topology(c1)
 t.topology()
