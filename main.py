16725abda928f34568417847504ef65e6ef49c78
Ilker Manap <ilkermanap@gmail.com>
2023-11-08 10:05:31 +0100

    Added argument parsing. We can provide the output from file or get it by running the command.

--- main.py
+++ main.py
@@ -1,6 +1,34 @@
+import argparse
+import sys
+import subprocess
 from topology import Topology
 
-c1 = open("ibnetdiscover-output.txt").readlines()
 
-t = Topology(c1)
-t.topology()
+parser = argparse.ArgumentParser()
+parser.add_argument("--file",
+                    help="File name for ibnetdiscover command output",
+                    action="store")
+parser.add_argument("--run",
+                    help="Get information by running ibnetdiscover",
+                    action="store_true")
+
+
+args = parser.parse_args()
+
+if args.file:
+    try:
+        contents = open(args.file).readlines()
+        topology = Topology(contents)
+        topology.topology()
+    except FileNotFoundError:
+        print(f"There is no file named {args.file}")
+        sys.exit(1)
+
+if args.run:
+    try:
+        result = subprocess.run(["ibnetdiscover"], capture_output=True, text=True)
+        topology = Topology(result.stdout)
+        topology.topology()
+    except FileNotFoundError:
+        print("Make sure that ibnetdiscover is installed")
+        sys.exit(2)
